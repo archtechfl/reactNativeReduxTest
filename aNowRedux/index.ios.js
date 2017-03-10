4e52f3e233dd386565752d768f13dabe634df7be
Jeremy Moore <archtechfl@gmail.com>
2017-03-10 14:51:53 -0500

    geolocation online

--- aNowRedux/index.ios.js
+++ aNowRedux/index.ios.js
@@ -5,7 +5,62 @@ class HelloWorldApp extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>Hello world!</Text>
+        <Text style={styles.text}>Finding next departure for nearest stop</Text>
+        <GeoLocation/>
+      </View>
+    );
+  }
+}
+
+class GeoLocation extends React.Component {
+  state = {
+    initialPosition: 'unknown',
+    lastPosition: 'unknown',
+  };
+
+  watchID: ?number = null;
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        var initialPosition = position;
+        this.setState({initialPosition});
+      },
+      (error) => alert(JSON.stringify(error)),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      var lastPosition = position;
+      this.setState({lastPosition});
+    });
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
+
+  displayPosition() {
+    let position = this.state.initialPosition.coords;
+    if (this.state.lastPosition !== "unknown") {
+        position = this.state.lastPosition.coords;
+    } else if (this.state.initialPosition !== "unknown") {
+        position = this.state.initialPosition.coords;
+    } else {
+        position = {
+            "latitude": 0,
+            "longitude": 0,
+        };
+    }
+    return `${position.latitude}, ${position.longitude}`;
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>
+          <Text style={styles.title}>Initial position: </Text>
+          {this.displayPosition()}
+        </Text>
       </View>
     );
   }
@@ -19,6 +74,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center', 
     alignItems: 'center', 
   },
+  title: {
+    fontSize: 14
+  }
 });
 
 AppRegistry.registerComponent('aNowRedux', () => HelloWorldApp);
